806d8dad7caaf1994fbe7dbf62f9398abcb69f6b
SyedSabir895 <sabirsyed2005@gmail.com>
2024-07-13 10:23:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+//FOR loop : repeat some code for a limited amount of time
+
+/*for(let i=1; i<=10; i++){
+    console.log(i);
+
+}
+*/
+//console.log("Happy birthday!");
+
+/*for(let i=1; i<=10; i+=2){
+    console.log(i);
+
+} 
+
+for(let i=10; i>0; i--){
+    console.log(i);
+}
+
+for(let i=10; i>0; i-=2){
+    console.log(i);
+
+} */
+
+for(let i=1; i<=20; i++){
+if(i==13){
+    break;
+}
+else{
+    console.log(i);
+}
+}
